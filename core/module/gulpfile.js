27e0b4ab016fca319a8f7bdf7cedaaf5b6349ca0
josiahhaswell <josiah.haswell@gmail.com>
2018-01-15 15:30:33 -0700

    Moving to module directory

--- core/module/gulpfile.js
+++ core/module/gulpfile.js
@@ -2,6 +2,6 @@ var gulp = require('gulp');
 
 // all gulp tasks are located in the ./build/tasks directory
 // gulp configuration is in files in ./build directory
-require('require-dir')('build/tasks');
+require('require-dir')('gulp/tasks');
 
 gulp.task('default', ['watch']);
