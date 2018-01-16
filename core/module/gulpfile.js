857151ce457defa5c00f32e4e792992d9f41e87f
josiahhaswell <josiah.haswell@gmail.com>
2018-01-15 21:09:15 -0700

    Recreating atmosphere with webpack

--- core/module/gulpfile.js
+++
@@ -1,7 +0,0 @@
-var gulp = require('gulp');
-
-// all gulp tasks are located in the ./build/tasks directory
-// gulp configuration is in files in ./build directory
-require('require-dir')('gulp/tasks');
-
-gulp.task('default', ['watch']);
--- web/src/test/java/io/sunshower/atmosphere/ClassPathTest.java
+++ web/src/test/java/io/sunshower/atmosphere/ClassPathTest.java
@@ -1,5 +1,6 @@
 package io.sunshower.atmosphere;
 
+import io.sunshower.test.common.TestClasspath;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
@@ -8,21 +9,32 @@ import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.io.File;
+
 @RunWith(Arquillian.class)
 public class ClassPathTest {
+
+  static File file(String path) {
+    return new File(TestClasspath.buildDir().getParentFile(), path);
+  }
 
   @Deployment
   public static WebArchive createDeployment() {
     return ShrinkWrap.create(WebArchive.class)
-        .addAsWebInfResource("webapp/WEB-INF/jboss-deployment-structure.xml")
+        .addAsWebInfResource(file("src/main/webapp/WEB-INF/web.xml"))
+        .addAsWebInfResource(file("src/main/webapp/WEB-INF/jboss-web.xml"))
+        .addAsWebInfResource(file("src/test/webapp/WEB-INF/jboss-deployment-structure.xml"))
         .addClass(ClassPathTest.class)
+        .addClass(TestClasspath.class)
         .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
   }
 
   @Test
-  public void ensureLoadingClassFromDependentModuleWorks() throws ClassNotFoundException {
+  public void ensureLoadingClassFromDependentModuleWorks()
+      throws ClassNotFoundException, InterruptedException {
     System.out.println(getClass().getClassLoader());
     Class<?> clazz = getClass().getClassLoader().loadClass("io.sunshower.web.SunshowerCore");
-    System.out.println(clazz);
+
+    //    Thread.sleep(1000000);
   }
 }
