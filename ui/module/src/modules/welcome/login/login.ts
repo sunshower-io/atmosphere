ef83dbc2cd26e9b65ef534c73db2eba9b64f9279
Lisa Gumerman <lisajgumerman@gmail.com>
2018-01-25 22:53:10 -0700

    Attempting to UX-ify login/activation

--- ui/module/src/modules/welcome/login/login.ts
+++ ui/module/src/modules/welcome/login/login.ts
@@ -1,37 +1,27 @@
-import {autoinject} from "aurelia-dependency-injection";
 import {I18N} from "aurelia-i18n";
 import {SecurityService, User} from "aire/api/security";
-import {Aurelia} from "aurelia-framework";
+import {Aurelia, autoinject} from "aurelia-framework";
 import {Router} from "aurelia-router";
-import {configureClient} from "init/configure-plugins";
-import {set, StorageMode} from "aire/api/storage";
+import {AuthenticationPage} from "../authentication-page";
 
 
 @autoinject
-export class Login {
+export class Login extends AuthenticationPage {
 
     private user: User = new User();
 
     constructor(private locale: I18N,
-                private router: Router,
-                private aurelia: Aurelia,
-                private securityService: SecurityService,) {
+                router: Router,
+                aurelia: Aurelia,
+                securityService: SecurityService) {
+        super(router, aurelia, securityService);
     }
 
     attached() {
     }
 
     async login(): Promise<void> {
-        let authentication = await this.securityService.login(this.user);
-        set(
-            "sunshower-auth-token", 
-            authentication.token.value, 
-            StorageMode.Local
-        );
-        await this.securityService.authenticateByToken(authentication.token);
-        this.router.navigate('/', {replace: true, trigger: false});
-        configureClient(this.aurelia, authentication.token.value);
-        await this.aurelia.setRoot('modules/main/index');
+        return this.authenticate(this.user);
     }
 
 
