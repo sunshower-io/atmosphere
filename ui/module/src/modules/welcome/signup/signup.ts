88d55a55889ba1623068fd840aa974b62037afaa
josiahhaswell <josiah.haswell@gmail.com>
2018-01-28 19:06:11 -0700

    Adding i18n

--- ui/module/src/modules/welcome/signup/signup.ts
+++ ui/module/src/modules/welcome/signup/signup.ts
@@ -1,6 +1,7 @@
 import {autoinject} from "aurelia-dependency-injection";
 import {SecurityService, User} from "aire/api/security";
 import {Aurelia} from "aurelia-framework";
+import {I18N} from "aurelia-i18n";
 
 @autoinject
 export class Signup {
@@ -8,9 +9,9 @@ export class Signup {
     private user: User = new User();
 
     constructor(
+        private i18n: I18N,
         private securityService: SecurityService
     ) {
-        
         
     }
 
